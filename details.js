49ef3b6725312374ccc1d478a3c3b3366262d1e6
jozef-wolf <jozefwolf@o2.pl>
2021-03-13 13:51:22 +0100

    details rendered

--- details.js
+++ details.js
@@ -1,3 +1,16 @@
+//js for details
+const id = new URLSearchParams(window.location.search).get("id");
+const container = document.querySelector(".details");
 
+const renderDetails = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/" + id);
+  const post = await res.json();
+  console.log(post);
+
+  const template = `<div>
+  <h1>${post.title}</h1><p>${post.body}</p></div>`;
+
+  container.innerHTML = template;
+};
 
 window.addEventListener("DOMContentLoaded", () => renderDetails());
