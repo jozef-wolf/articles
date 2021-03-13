1c9bbf2e06096683dc406ccab8600ec545a29872
jozef-wolf <jozefwolf@o2.pl>
2021-03-13 13:21:38 +0100

    posts rendered

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 const container = document.querySelector(".container__posts");
+
+//create render function
+
+const renderPosts = async () => {
+  let uri = `                                    
+https://jsonplaceholder.typicode.com/posts?_sort=title
+`;
+  const res = await fetch(uri); //wait till we get the data (response object) back (response is stored in const).
+  const posts = await res.json(); //json takes the response object and parses the data into js object.
+  console.log(posts);
+
+  let template = "";
+  posts.forEach((post) => {
+    //cirlce through posts and fire a callback function for each post. Each time we fire callback fn we get access.
+    template += `
+    <div class='post'>
+    <h2 class='title'>${post.title}</h2>
+    <p class='article'>${post.body.slice(0, 200)}</p>
+    <a href="/details.html?id=${post.id}">read more..</a>
+    </div>`;
+  });
+  container.innerHTML = template;
+};
+
+//wait till DOM content is loaded, then fire a function.
+
+window.addEventListener("DOMContentLoaded", () => renderPosts());
