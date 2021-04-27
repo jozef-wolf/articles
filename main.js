f44d812f20195f782d728644c8ac2d94e93cf8e6
jozef-wolf <wolf.joozef@gmail.com>
2021-04-27 12:46:46 +0200

    render weather inside geolocation

--- main.js
+++ main.js
@@ -2,68 +2,64 @@ const postContainer = document.querySelector(".container__posts");
 const asideContainer = document.querySelector(".container__weather");
 //create render function
 
-// const renderPosts = async () => {
-//   let url = `
-// https://jsonplaceholder.typicode.com/posts?_sort=title
-// `;
-//   const res = await fetch(url); //wait till we get the data (response object) back (response is stored in const).
+const renderPosts = async () => {
+  let url = `
+https://jsonplaceholder.typicode.com/posts?_sort=title
+`;
+  const res = await fetch(url); //wait till we get the data (response object) back (response is stored in const).
 
-//   const posts = await res.json(); //json takes the response object and parses the data into js object.
-//   console.log(posts);
-
-//   let template = "";
-//   posts.forEach((post) => {
-//     //cirlce through posts and fire a callback function for each post. Each time we fire callback fn we get access.
-//     template += `
-//     <div class='post'>
-//     <h2 class='title'>${post.title}</h2>
-//     <p class='article'>${post.body.slice(0, 200)}</p>
-//     <a href="/details.html?id=${post.id}">read more..</a>
-//     </div>`;
-//   });
-//   postContainer.innerHTML = template;
-// };
-
-// window.addEventListener("DOMContentLoaded", () => renderPosts());
-
-let input = "Sopot";
-
-var lat, lon;
-var promise1 = new Promise(function (resolve, reject) {
-  navigator.geolocation.getCurrentPosition(function (pos) {
-    lat = pos.coords.latitude;
-    lon = pos.coords.longitude;
-    resolve({ lat, lon });
-  });
-});
-
-promise1.then(function (value) {
-  console.log(value.lat, value.lon);
-});
-
-const renderWeather = async () => {
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=b9c60fdee21abc84b0d8ef6f3ec39b79`;
-
-  // let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=b9c60fdee21abc84b0d8ef6f3ec39b79`;
-  const res = await fetch(url);
-  const weater = await res.json();
-  console.log(weater);
-
-  let pic = weater.weather[0].icon;
-  let icon = `http://openweathermap.org/img/wn/${pic}.png`;
+  const posts = await res.json(); //json takes the response object and parses the data into js object.
+  console.log(posts);
 
   let template = "";
-  function convertToC(kelvin) {
-    let celsius = kelvin - 273.15;
-    return celsius;
-  }
+  posts.forEach((post) => {
+    //cirlce through posts and fire a callback function for each post. Each time we fire callback fn we get access.
+    template += `
+    <div class='post'>
+    <h2 class='title'>${post.title}</h2>
+    <p class='article'>${post.body.slice(0, 200)}</p>
+    <a href="/details.html?id=${post.id}">read more..</a>
+    </div>`;
+  });
+  postContainer.innerHTML = template;
+};
 
-  let celsiusTemp = convertToC(weater.main.temp).toFixed(1);
-  let celsiusFeel = convertToC(weater.main.feels_like).toFixed(2);
+window.addEventListener("DOMContentLoaded", () => renderPosts());
 
-  //cirlce through posts and fire a callback function for each post. Each time we fire callback fn we get access.
+let input = "Sopot";
+function showPosition() {
+  navigator.geolocation.getCurrentPosition(showMap);
+}
 
-  template += `
+function showMap(position) {
+  // Get location data
+  var lat = position.coords.latitude;
+  var long = position.coords.longitude;
+  console.log(lat, long);
+
+  const renderWeather = async () => {
+    // let url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=b9c60fdee21abc84b0d8ef6f3ec39b79`;
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=b9c60fdee21abc84b0d8ef6f3ec39b79`;
+    const res = await fetch(url);
+    const weater = await res.json();
+    console.log(weater);
+
+    let pic = weater.weather[0].icon;
+    let icon = `http://openweathermap.org/img/wn/${pic}.png`;
+
+    let template = "";
+    function convertToC(kelvin) {
+      let celsius = kelvin - 273.15;
+      return celsius;
+    }
+
+    let celsiusTemp = convertToC(weater.main.temp).toFixed(1);
+    let celsiusFeel = convertToC(weater.main.feels_like).toFixed(2);
+
+    //cirlce through posts and fire a callback function for each post. Each time we fire callback fn we get access.
+
+    template += `
       <h2 class='container__weather--city'>${weater.name} ${weater.sys.country}</h2>
       <div class='container__weather--icon'><img src='${icon}'></img></div>
       <ul class='container__weather--info'>
@@ -75,8 +71,12 @@ const renderWeather = async () => {
       </ul>
     `;
 
-  asideContainer.innerHTML = template;
-};
+    asideContainer.innerHTML = template;
+  };
+  renderWeather();
+}
+showPosition();
+
 //wait till DOM content is loaded, then fire a function.
 
-window.addEventListener("DOMContentLoaded", () => renderWeather());
+// window.addEventListener("DOMContentLoaded", () => renderWeather());
